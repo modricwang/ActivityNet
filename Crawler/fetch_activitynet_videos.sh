5c445bbd8d950a3534f1507ee6a6de054fac815b
Modric Wang <modricwang@126.com>
2017-12-19 01:22:17 +0800

    add split code

--- Crawler/fetch_activitynet_videos.sh
+++ Crawler/fetch_activitynet_videos.sh
@@ -2,19 +2,21 @@
 
 VIDEOPATH=$1
 JSON_FILE=$2
-TEMP_FILE="command_list.txt"
-
-if [ -d $VIDEOPATH ]; then
-    python run_crosscheck.py $VIDEOPATH $JSON_FILE $TEMP_FILE
-    if [ -f $TEMP_FILE ]; then
-        bash $TEMP_FILE
-    else
-        echo "File $TEMP_FILE does not exists."
-    fi
-else
-    echo "Directory does not exists."
-    exit 0
-fi
-
+for i in `seq 1 31`
+do
+    TEMP_FILE="command_list$i.txt"
+    echo $TEMP_FILE
+    # if [ -d $VIDEOPATH ]; then
+    #     python run_crosscheck.py $VIDEOPATH $JSON_FILE $TEMP_FILE
+    #     if [ -f $TEMP_FILE ]; then
+    #         bash $TEMP_FILE
+    #     else
+    #         echo "File $TEMP_FILE does not exists."
+    #     fi
+    # else
+    #     echo "Directory does not exists."
+    #     exit 0
+    # fi
+done
 #rm $TEMP_FILE
 echo "Have a good day!"
---
+++ Crawler/split.py
@@ -0,0 +1,17 @@
+import json
+
+tot_cnt = 31
+input_json_path = "activity_net.v1-3.min.json"
+data = open(input_json_path, 'r')
+data = json.load(data)
+index = 0
+out_list = [dict() for i in range(tot_cnt)]
+for key in data["database"]:
+    # print data["database"][key]
+    out_list[index].setdefault(key, data["database"][key])
+    index += 1
+    if index >= tot_cnt:
+        index -= tot_cnt
+for i in range(tot_cnt):
+    with open('sublist/sublist%d.json' % (i), 'w') as f:
+        json.dump(out_list[i], f)
