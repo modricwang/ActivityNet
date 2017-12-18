00bb096fc48fe364aa840c4c97dc89fb2d948a15
Modric Wang <modricwang@126.com>
2017-12-19 01:25:49 +0800

    modify bash file for multithread download

--- Crawler/fetch_activitynet_videos.sh
+++ Crawler/fetch_activitynet_videos.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-VIDEOPATH=$1
-JSON_FILE=$2
+VIDEOPATH=video/
+
+mkdir temp
 for i in `seq 1 31`
 do
-    TEMP_FILE="command_list$i.txt"
-    echo $TEMP_FILE
-    # if [ -d $VIDEOPATH ]; then
-    #     python run_crosscheck.py $VIDEOPATH $JSON_FILE $TEMP_FILE
-    #     if [ -f $TEMP_FILE ]; then
-    #         bash $TEMP_FILE
-    #     else
-    #         echo "File $TEMP_FILE does not exists."
-    #     fi
-    # else
-    #     echo "Directory does not exists."
-    #     exit 0
-    # fi
+    TEMP_FILE="temp/operate$i.txt"
+    JSON_FILE="sublist/sublist$i.json"
+    if [ -d $VIDEOPATH ]; then
+        python run_crosscheck.py $VIDEOPATH $JSON_FILE $TEMP_FILE
+        if [ -f $TEMP_FILE ]; then
+            bash $TEMP_FILE
+        else
+            echo "File $TEMP_FILE does not exists."
+        fi
+    else
+        echo "Directory does not exists."
+        exit 0
+    fi
 done
 #rm $TEMP_FILE
 echo "Have a good day!"
